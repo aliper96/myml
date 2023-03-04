772a70131a173cd0f90f6207d61c19afb25f558f
aliper <ali.h.amza@hotmail.com>
2023-03-04 20:32:45 +0100

    merging changes

---
+++ test_Value.py
@@ -0,0 +1,54 @@
+from Value import Value
+from drawer import draw_dot
+
+
+def first_test():
+    # inputs x1,x2
+    x1 = Value(2.0, label='x1')
+    x2 = Value(0.0, label='x2')
+    # weights w1,w2
+    w1 = Value(-3.0, label='w1')
+    w2 = Value(1.0, label='w2')
+    # bias of the neuron
+    b = Value(6.8813735870195432, label='b')
+    # x1*w1 + x2*w2 + b
+    x1w1 = x1*w1; x1w1.label = 'x1*w1'
+    x2w2 = x2*w2; x2w2.label = 'x2*w2'
+    x1w1x2w2 = x1w1 + x2w2; x1w1x2w2.label = 'x1*w1 + x2*w2'
+    n = x1w1x2w2 + b; n.label = 'n'
+    o = n.tanh(); o.label = 'o'
+    o.backward()
+    dot = draw_dot(o)
+    dot.render(filename='first_test', directory='images', cleanup=True, view=False)
+
+
+def second_test():
+    # inputs x1,x2
+    x1 = Value(2.0, label='x1')
+    x2 = Value(0.0, label='x2')
+    # weights w1,w2
+    w1 = Value(-3.0, label='w1')
+    w2 = Value(1.0, label='w2')
+    # bias of the neuron
+    b = Value(6.8813735870195432, label='b')
+    # x1*w1 + x2*w2 + b
+    x1w1 = x1 * w1;
+    x1w1.label = 'x1*w1'
+    x2w2 = x2 * w2;
+    x2w2.label = 'x2*w2'
+    x1w1x2w2 = x1w1 + x2w2;
+    x1w1x2w2.label = 'x1*w1 + x2*w2'
+    n = x1w1x2w2 + b;
+    n.label = 'n'
+    # ----
+    e = (2 * n).exp()
+    o = (e - 1) / (e + 1)
+    # ----
+    o.label = 'o'
+    o.backward()
+    dot = draw_dot(o)
+    dot.render(filename='second_test', directory='images', cleanup=True, view=False)
+
+
+first_test()
+second_test()
